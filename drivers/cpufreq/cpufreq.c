a0b22159aa1aa741e9f0eaf1881d650def01f603
SamarV-121 <samarvispute121@gmail.com>
2018-03-27 15:05:44 +0530

    sprout: cpufreq: update
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1589,17 +1589,22 @@ EXPORT_SYMBOL_GPL(cpufreq_driver_target);
 
 int __cpufreq_driver_getavg(struct cpufreq_policy *policy, unsigned int cpu)
 {
- int ret = 0;
+	int ret = 0;
 
- policy = cpufreq_cpu_get(policy->cpu);
- if (!policy)
- return -EINVAL;
+	if (cpufreq_disabled())
+		return ret;
 
- if (cpu_online(cpu) && cpufreq_driver->getavg)
- ret = cpufreq_driver->getavg(policy, cpu);
+	if (!cpufreq_driver->getavg)
+		return 0;
 
- cpufreq_cpu_put(policy);
- return ret;
+	policy = cpufreq_cpu_get(policy->cpu);
+	if (!policy)
+		return -EINVAL;
+
+	ret = cpufreq_driver->getavg(policy, cpu);
+
+	cpufreq_cpu_put(policy);
+	return ret;
 }
 EXPORT_SYMBOL_GPL(__cpufreq_driver_getavg);
 
